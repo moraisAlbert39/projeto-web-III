d14b685285f1b10b768c1ae3eb01794399631377
moraisAlbert39 <64670482+moraisAlbert39@users.noreply.github.com>
2022-10-05 10:47:37 -0300

    Update serve.js

--- src/serve.js
+++ src/serve.js
@@ -1,5 +1,6 @@
 const app = require('./app');
-
+const mongoose = require("mongoose");
+mongoose.connect('mongodb+srv://admin:aem123@projetoweb.ptjyzvs.mongodb.net/?retryWrites=true&w=majority').then(() => console.log('servidor conectado com o banco de dados')).catch((err) => console.log(err));
 
 
 app.listen(999, () => console.log('servidor rodando'));
